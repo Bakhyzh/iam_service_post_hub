d1943a5b1076b869a3042d02f3325bd415f6cb86
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-05-30 23:17:00 +0500

    adding get post

--- src/main/java/com/post_hub/iam_service/Controller/PostController.java
+++ src/main/java/com/post_hub/iam_service/Controller/PostController.java
@@ -1,4 +1,18 @@
 package com.post_hub.iam_service.Controller;
 
+import com.post_hub.iam_service.model.entity.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("posts")
 public class PostController {
+    @GetMapping
+    public ResponseEntity<Post> getPosts(){
+        Post post = new Post();
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
 }
