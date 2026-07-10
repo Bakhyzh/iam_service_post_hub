dfdeb53ae280eaaaf32e6c47d4d7035c50bcbad0
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-07-10 23:44:24 +0500

    testing getallusers with Integration test

---
+++ src/test/java/com/post_hub/iam_service/integration/user_controller/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.post_hub.iam_service.integration.user_controller;
+
+import com.post_hub.iam_service.IamServiceApplication;
+import com.post_hub.iam_service.model.entity.User;
+import com.post_hub.iam_service.model.exception.NotFoundException;
+import com.post_hub.iam_service.repositories.UserRepository;
+import lombok.Setter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.ObjectMapper;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(classes = IamServiceApplication.class)
+@AutoConfigureMockMvc
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+public class UserControllerTest {
+
+    @Autowired @Setter
+    private MockMvc mockMvc;
+    @Autowired @Setter
+    private UserRepository userRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    @Transactional
+    void setUp(){
+        User user = userRepository.findById(1)
+                .orElseThrow(()->new NotFoundException("not found"));
+    }
+
+    @Test
+    @Transactional
+    void getAllUsers_200_OK() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/users/all")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
