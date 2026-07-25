4ea77416e40b7a62d0107f8fd63c31229dfe9bc1
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-07-25 22:30:23 +0500

    adding NewCommentRequest

---
+++ src/main/java/com/post_hub/iam_service/model/request/comment/NewCommentRequest.java
@@ -0,0 +1,23 @@
+package com.post_hub.iam_service.model.request.comment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewCommentRequest implements Serializable {
+    @NotBlank(message = "title cannot be empty ")
+    private String title;
+    @NotBlank(message = "content cannot be empty")
+    private String content;
+    @NotNull(message = "Specify the number of likes")
+    private Integer likes;
+
+
+}
