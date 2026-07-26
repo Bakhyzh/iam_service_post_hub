0912b2f18519a355225ef4085db6a1b152c98431
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-07-26 11:06:34 +0500

    change imports

--- src/main/java/com/post_hub/iam_service/service/CommentService.java
+++ src/main/java/com/post_hub/iam_service/service/CommentService.java
@@ -1,6 +1,6 @@
 package com.post_hub.iam_service.service;
 
-import com.post_hub.iam_service.model.dto.post.CommentDTO;
+import com.post_hub.iam_service.model.dto.comment.CommentDTO;
 import com.post_hub.iam_service.model.entity.Comment;
 import com.post_hub.iam_service.model.request.comment.NewCommentRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
