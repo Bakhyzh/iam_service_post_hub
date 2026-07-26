1e87e9f75f79fadcfd4e5cc78483030ebe50fcee
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-07-26 11:08:30 +0500

    di-constructor practice

--- src/main/java/com/post_hub/iam_service/service/CommentService.java
+++ src/main/java/com/post_hub/iam_service/service/CommentService.java
@@ -1,12 +1,9 @@
 package com.post_hub.iam_service.service;
 
 import com.post_hub.iam_service.model.dto.comment.CommentDTO;
-import com.post_hub.iam_service.model.entity.Comment;
 import com.post_hub.iam_service.model.request.comment.NewCommentRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 public interface CommentService {
     IamResponse<CommentDTO> createComment(@NotNull NewCommentRequest request);
