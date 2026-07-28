7b40071afcc4ce391f74232c7a67a7ba301b43a0
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-07-28 23:00:29 +0500

    CommentMapper modified

--- src/main/java/com/post_hub/iam_service/mapper/CommentMapper.java
+++ src/main/java/com/post_hub/iam_service/mapper/CommentMapper.java
@@ -1,10 +1,11 @@
 package com.post_hub.iam_service.mapper;
 
+import com.post_hub.iam_service.model.dto.comment.CommentDTO;
 import com.post_hub.iam_service.model.dto.post.PostDTO;
 import com.post_hub.iam_service.model.dto.post.PostSearchDTO;
 import com.post_hub.iam_service.model.entity.Post;
 import com.post_hub.iam_service.model.entity.User;
-import com.post_hub.iam_service.model.request.post.NewPostRequest;
+import com.post_hub.iam_service.model.request.comment.NewCommentRequest;
 import com.post_hub.iam_service.model.request.post.UpdatePostRequest;
 import org.hibernate.type.descriptor.DateTimeUtils;
 import org.mapstruct.Mapper;
@@ -21,13 +22,13 @@ import java.util.Objects;
 )
 public interface CommentMapper {
 
-    PostDTO toPostDto(Post post);
+    CommentDTO toCommentDto(Post post);
 
     @Mapping(target = "id",ignore = true)
     @Mapping(target = "created",ignore = true)
     @Mapping(source = "user",target = "user")
     @Mapping(target = "createdBy",source= "user.username")
-    Post createPost(NewPostRequest newPostRequest, User user);
+    Post createComment(NewCommentRequest newRequest, User user);
     @Mapping(target = "id",ignore = true)
     @Mapping(target = "created",ignore = true)
     void updatePost(@MappingTarget Post post , UpdatePostRequest updatePostRequest);
