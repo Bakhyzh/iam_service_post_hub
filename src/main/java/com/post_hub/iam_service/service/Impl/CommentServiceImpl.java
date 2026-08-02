c45d24df4b43e2e879edc74febe820ad3e499e17
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-08-02 17:34:27 +0500

    fix

--- src/main/java/com/post_hub/iam_service/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/post_hub/iam_service/service/Impl/CommentServiceImpl.java
@@ -3,15 +3,12 @@ package com.post_hub.iam_service.service.Impl;
 import com.post_hub.iam_service.mapper.CommentMapper;
 import com.post_hub.iam_service.model.dto.comment.CommentDTO;
 import com.post_hub.iam_service.model.entity.Comment;
-import com.post_hub.iam_service.model.entity.Post;
 import com.post_hub.iam_service.model.entity.User;
 import com.post_hub.iam_service.model.request.comment.NewCommentRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
 import com.post_hub.iam_service.repositories.CommentRepository;
-import com.post_hub.iam_service.repositories.PostRepository;
 import com.post_hub.iam_service.repositories.UserRepository;
 import com.post_hub.iam_service.service.CommentService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
