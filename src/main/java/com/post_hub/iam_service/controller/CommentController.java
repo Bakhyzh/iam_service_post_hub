33b64f95113253d78be1f03cc58c024127aedc64
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-08-04 13:32:02 +0500

    fixes@2

--- src/main/java/com/post_hub/iam_service/controller/CommentController.java
+++ src/main/java/com/post_hub/iam_service/controller/CommentController.java
@@ -1,7 +1,5 @@
 package com.post_hub.iam_service.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
